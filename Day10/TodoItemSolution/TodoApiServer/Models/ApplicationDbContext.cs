1d05ca1d1e02907855234ef1352389734c48b450
kooweajeeI <leejw2873@naver.com>
2023-06-01 10:02:20 +0900

    0531

---
+++ Day10/TodoItemSolution/TodoApiServer/Models/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApiServer.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
